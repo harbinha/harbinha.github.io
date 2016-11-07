e232b50cc387f9dc7bac5533d92aa0a299b7a1b4
Hunter Harbin <hharbin@gannett.com>
2016-11-07 16:34:56 -0500

    make a call

--- src/foo/foobar.vue
+++ src/foo/foobar.vue
@@ -1,13 +1,32 @@
 <template>
     <div>
         <p>{{ message }}</p>
+        {{ data }}
     </div>
 </template>
 <script>
 export default{
     data() {
         return{
-            message : 'it\'s all foobar'
+            message : 'it\'s all foobar',
+            data: {}
+        }
+    },
+    created() {
+        console.log('ready');
+        this.getFoo();
+    },
+
+    methods: {
+        getFoo: function() {
+            console.log('fetching')
+            fetch('https://radiant-ridge-45919.herokuapp.com/times').then(response => {
+                response.json().then(res => {
+                    console.log(res);
+                    this.data = res;
+                })
+
+            })
         }
     }
 }
