87d74505d6f6e2c0ba215b030bb6d51eb957f5f2
Hunter Harbin <hharbin@gannett.com>
2016-11-18 17:36:16 -0500

    firebase support

---
+++ public/src/foo/foobar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+export default{
+    data() {
+        return{
+            message : 'it\'s all foobar',
+        }
+    },
+    created() {
+        console.log('ready');
+    },
+
+    methods: {}
+}
+</script>
+<style>
+</style>
